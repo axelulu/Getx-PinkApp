4e7ef5ad72140adcf50b16532eff7d1561988f9d
Coder-ZhaoLu <izhaicy@163.com>
2022-04-06 12:52:43 +0800

    适配最新版本

--- lib/pink_constants.dart
+++ lib/pink_constants.dart
@@ -2,9 +2,9 @@ import 'package:pink_acg/app/http/dao/login_dao.dart';
 
 /// 全局config配置文件
 class PinkConstants {
-  static String domain = "10.0.2.2";
+  // static String domain = "10.0.2.2";
   // static String domain = "localhost";
-  // static String domain = "81.69.27.9";
+  static String domain = "81.69.27.9";
   static String ossDomain = "https://img.catacg.cn";
   static String port = "8080";
   static String versionPath = "/api/v1";
